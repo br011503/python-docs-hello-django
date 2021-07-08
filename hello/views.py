806c54ef374f9b8195021859170e20d05c45761b
br011503 <49256179+br011503@users.noreply.github.com>
2021-07-08 15:04:36 +0900

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def hello(request):
-    return HttpResponse("Hello, World!")
 # def hello(request):
-#     return render(request, 'table1.html')
+#     return HttpResponse("Hello, World!")
+def hello(request):
+    return render(request, 'templates/hello/table1.html')
