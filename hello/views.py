5a492423834af97f2724ca99e5727731e6dfc950
br011503 <49256179+br011503@users.noreply.github.com>
2021-07-12 08:38:23 +0900

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -5,4 +5,42 @@ from django.shortcuts import render
 #     return HttpResponse("Hello, World!")
 
 def index(request):
-    return render(request, "tables/table1.html")
+    import urllib.request
+    import json
+    import os
+    import ssl
+    import pandas as pd
+
+    def allowSelfSignedHttps(allowed):
+        # bypass the server certificate verification on client side
+        if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
+            ssl._create_default_https_context = ssl._create_unverified_context
+
+    allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+
+    # Request data goes here
+    data = {'param':{'bld':1}}
+
+    body = str.encode(json.dumps(data))
+
+    url = 'http://52.141.0.146:80/api/v1/service/tsop-skt-ocb-control/score'
+    api_key = 'kwea4NGIHAlBO1g5P6M4fQ5dVSb2D5Lz' # Replace this with the API key for the web service
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+
+    req = urllib.request.Request(url, body, headers)
+
+    try:
+        response = urllib.request.urlopen(req)
+
+        result = response.read()
+        result = json.loads(result.decode("utf-8"))
+        print(result)
+
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
+
+    table = json.loads(result['table'])
+    table['Tcount'] = result['count']
+    table['time'] = result['time']
+    context = table
+    return render(request, "tables/table1.html", context)
