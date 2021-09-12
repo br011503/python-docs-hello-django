691274c4a33b879ed0b13e25dc2ac601abc0339e
br011503 <49256179+br011503@users.noreply.github.com>
2021-09-13 07:09:11 +0900

    Update urls.py

--- hello/urls.py
+++ hello/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [    
-#     path('', views.hello, name='hello'),
     url(r'^$', views.index, name='index'),
+    url('enthalpy_control/', views.enthalpy_control, name='enthalpy_control'),
+    url('oa_control/', views.oa_control, name='oa_control'),
+    url('ahu_optimal_control/', views.ahu_optimal_control, name='ahu_optimal_control'),
 ]
