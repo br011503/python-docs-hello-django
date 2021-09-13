6de011731ea9d71dee143c2146e982a8027ea5eb
br011503 <49256179+br011503@users.noreply.github.com>
2021-09-13 13:44:56 +0900

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -17,24 +17,22 @@ def index(request):
     allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
     data = {'param':{'bld':1}}
     body = str.encode(json.dumps(data))
-    url = 'http://52.141.0.146:80/api/v1/service/tsop-skt-ocb-control/score'
-    api_key = 'kwea4NGIHAlBO1g5P6M4fQ5dVSb2D5Lz' # Replace this with the API key for the web service
+    url = 'http://52.141.0.146:80/api/v1/service/tsop-skt-tower-main/score'
+    api_key = 'JbixWfRonFXDehmdbHcPit4rbyZJpsog' # Replace this with the API key for the web service
     headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
     req = urllib.request.Request(url, body, headers)
     try:
         response = urllib.request.urlopen(req)
         result = response.read()
-        result = json.loads(result.decode("utf-8"))
     except urllib.error.HTTPError as error:
         print("The request failed with status code: " + str(error.code))
-    values = json.loads(result['table'])
-    values['Tcount'] = result['count']
-    values['time'] = result['time']
-    values['pm10'] = int(result['pm10'])
-    values['index_pm10'] = result['index_pm10']
-    values['pm25'] = int(result['pm25'])
-    values['index_pm25'] = result['index_pm25']
-    return render(request, "tables/index.html", context = values)
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        print(error.info())
+        print(json.loads(error.read().decode("utf8", 'ignore')))
+
+    result = json.loads(result)
+    result = json.loads(result['df1'])
+    return render(request, "tables/index.html", context = result)
 
 def oa_control(request):
     allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
