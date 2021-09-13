a37841a3e0a12d69c20f8302c39ab639153268c1
br011503 <49256179+br011503@users.noreply.github.com>
2021-09-13 10:09:05 +0900

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -56,7 +56,22 @@ def oa_control(request):
     return render(request, "tables/oa_control.html", context = values)
 
 def enthalpy_control(request):
-    return render(request, "tables/enthalpy_control.html")
+    allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+    data = {'param': {'bld': '1' }}
+    body = str.encode(json.dumps(data))
+    url = 'http://52.141.0.146:80/api/v1/service/tsop-skt-ahu-economizer/score'
+    api_key = 'UrAzVhOgxF0xvmYeWHN48CqaVf42Fx5L' # Replace this with the API key for the web service
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+    req = urllib.request.Request(url, body, headers)
+    try:
+        response = urllib.request.urlopen(req)
+        result = response.read()
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
+        print(error.info())
+    values = json.loads(result)
+    values['table'] = json.loads(values['table'])
+    return render(request, "tables/enthalpy_control.html", context = values)
 
 def ahu_optimal_control(request):
     return render(request, "tables/ahu_optimal_control.html")
