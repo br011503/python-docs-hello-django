95c1c10bb879ee205cc1cf2645086dee15e3b141
br011503 <49256179+br011503@users.noreply.github.com>
2021-09-13 09:20:17 +0900

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -13,12 +13,7 @@ def allowSelfSignedHttps(allowed):
     if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
         ssl._create_default_https_context = ssl._create_unverified_context
 
-
-# def hello(request):
-#     return HttpResponse("Hello, World!")
-
 def index(request):
-
     allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
     data = {'param':{'bld':1}}
     body = str.encode(json.dumps(data))
@@ -26,15 +21,12 @@ def index(request):
     api_key = 'kwea4NGIHAlBO1g5P6M4fQ5dVSb2D5Lz' # Replace this with the API key for the web service
     headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
     req = urllib.request.Request(url, body, headers)
-
     try:
         response = urllib.request.urlopen(req)
         result = response.read()
         result = json.loads(result.decode("utf-8"))
-
     except urllib.error.HTTPError as error:
         print("The request failed with status code: " + str(error.code))
-
     values = json.loads(result['table'])
     values['Tcount'] = result['count']
     values['time'] = result['time']
@@ -52,15 +44,12 @@ def oa_control(request):
     api_key = 'kwea4NGIHAlBO1g5P6M4fQ5dVSb2D5Lz' # Replace this with the API key for the web service
     headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
     req = urllib.request.Request(url, body, headers)
-
     try:
         response = urllib.request.urlopen(req)
         result = response.read()
         result = json.loads(result.decode("utf-8"))
-
     except urllib.error.HTTPError as error:
         print("The request failed with status code: " + str(error.code))
-
     values = json.loads(result['table'])
     values['Tcount'] = result['count']
     values['time'] = result['time']
