91f0ffdf38571bfa568f2d8cb8f4597d4e1e98d1
br011503 <49256179+br011503@users.noreply.github.com>
2023-07-18 17:18:33 +0900

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -27,10 +27,6 @@ def index(request):
         result = json.loads(result)
         result = json.loads(result['df1'])
     except urllib.error.HTTPError as error:
-        print("The request failed with status code: " + str(error.code))
-        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-        print(error.info())
-        print(json.loads(error.read().decode("utf8", 'ignore')))
         result = '{"dbt":{"0":0.0},"rh":{"0":0.0},"wt":{"0":0.0},"x":{"0":0.0},"h":{"0":0.0},"PM10":{"0":"0"},"PM25":{"0":"0"},"time":{"0":"00월 00일 00시 00분"}}'
 
     
